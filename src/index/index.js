b32ea750589efa2da4485d1466311985028a8db4
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-02-22 14:15:31 -0800

    init commit

---
+++ src/index/index.js
@@ -0,0 +1,23 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+import '../utilities/styles/form.scss'
+import '../init.scss' ;
+import '../index/styles/topMenu.scss' ;
+import '../index/styles/search.scss' ;
+import '../index/styles/slider.scss' ;
+import '../index/styles/categories.scss' ;
+import '../index/styles/features.scss' ;
+import '../index/styles/carousel.scss' ;
+import '../index/styles/services.scss' ;
+import '../index/styles/residents.scss' ;
+import '../index/styles/articles.scss' ;
+import '../index/styles/sponsers.scss' ;
+import '../index/styles/footer.scss' ;
+//load scripts
+import '../index/scripts/topMenu.js' ;
+import '../index/scripts/search.js' ;
+import '../index/scripts/slider.js' ;
+import '../index/scripts/carousel.js' ;
+import '../index/scripts/sponsers.js' ;
+//load videos
