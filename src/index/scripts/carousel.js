42c0231e986e8fc533d2ef6c23be45b54e078e6f
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-02-25 21:49:34 +0330

    half on index

---
+++ src/index/scripts/carousel.js
@@ -0,0 +1,3 @@
+import Carousel from '../../utilities/scripts/sliderCarousel.js' ;
+new Carousel(document.querySelector('#discounts')) ;
+new Carousel(document.querySelector('#new')) ;
---
+++ src/index/scripts/slider.js
@@ -0,0 +1,2 @@
+import Slider from '../../utilities/scripts/sliderFade.js' 
+new Slider(document.querySelector('#topSlider'),4000) ;
