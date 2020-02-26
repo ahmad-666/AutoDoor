5562f16b5a60081d219e4c8f164e881452af2ef1
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-02-26 13:27:53 -0800

    full products

---
+++ src/products/products.js
@@ -0,0 +1,17 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../../node_modules/nouislider/distribute/nouislider.min.css' ;
+import '../framework.scss' ;
+import '../utilities/styles/form.scss'
+import '../init.scss' ;
+import '../index/styles/topMenu.scss' ;
+import '../index/styles/search.scss' ;
+import '../index/styles/slider.scss' ;
+import '../products/styles/products.scss' ;
+import '../index/styles/footer.scss' ;
+//load scripts
+import '../index/scripts/topMenu.js' ;
+import '../index/scripts/search.js' ;
+import '../index/scripts/slider.js' ;
+import '../products/scripts/products.js' ;
+//load videos
